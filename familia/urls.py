71a4fec81b56588f2939d3edb78b834c9eba125c
Mariano San Juan <marianosanjuan90@gmail.com>
2022-06-26 17:00:01 -0300

    Creacion de data base+Templates familiadb+Template familiadburl

--- familia/urls.py
+++ familia/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import familia, home
+from .views import familia, familia_db, familia_dburl, home
 
 urlpatterns = [
     path("", home),
-    path("mi-familia", familia)
+    path("mi-familia", familia),
+    path("mi-familiadb", familia_db),
+    path("mi-familiadburl/<integrante_nombre>/<integrante_edad>", familia_dburl)
+    
 ]
--- familia/views.py
+++ familia/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader, Template, Context
 from django.shortcuts import render
+from familia.models import Integrantesfamilia
 
 # Create your views here.
 # Creación de vista de familia
@@ -20,6 +21,31 @@ def familia(request):
     nombre = "Mariano"
     apellido = "San Juan"
     lista = ["Andrés","Eugenia","Carolina","Ricardo"]
-    render1 = template1.render({"nombre":nombre,"apellido":apellido,"lista": lista})
+    listaedad = [33,24,62,64]
+    
+    render1 = template1.render({"nombre":nombre,"apellido":apellido,"lista": lista,"listaedad":listaedad})
     return HttpResponse(render1)
 
+
+def familia_db(request):
+    
+    template1 = loader.get_template("familiadb.html")
+    nombre = "Mariano"
+    apellido = "San Juan"
+    
+    integrante = Integrantesfamilia(nombre = "Sofia", edad =29)
+    # integrante.save()
+    
+    render1 = template1.render({"nombre":nombre,"apellido":apellido,"integrante":integrante})
+    return HttpResponse(render1)
+
+def familia_dburl(request,integrante_nombre, integrante_edad):
+    
+    template1 = loader.get_template("familiadburl.html")
+    nombre = integrante_nombre
+    edad = integrante_edad
+    
+    # integrante.save()
+    
+    render1 = template1.render({"nombre":nombre,"edad":edad})
+    return HttpResponse(render1)
